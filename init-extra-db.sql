55f6708bd0ae5830bac143ca5e5316c5c4607793
arulb2w <arulb2w@gmail.com>
2026-04-11 19:38:39 +0530

    access issue fix

--- init-extra-db.sql
+++ init-extra-db.sql
@@ -8,4 +8,7 @@ GRANT ALL PRIVILEGES ON admin_inspiress.* TO 'admin_inspiress'@'%';
 CREATE USER IF NOT EXISTS 'edinztec_admin'@'%' IDENTIFIED BY 'Ke2FES@e4evxIUT';
 GRANT ALL PRIVILEGES ON edinztec_admin.* TO 'edinztec_admin'@'%';
 
+-- Ensure admin_edinz has access from any host
+GRANT ALL PRIVILEGES ON admin_edinz.* TO 'admin_edinz'@'%';
+
 FLUSH PRIVILEGES;
